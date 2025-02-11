9ab297e414db909d18e49e43fee34720fcf74753
Tram <tramnhoang@gmail.com>
2025-02-11 00:03:30 -0800

    Prepare for deployment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from datetime import timedelta
 from dotenv import load_dotenv
 import os
+import dj_database_url
 
 load_dotenv()
 
@@ -96,14 +97,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT"),
-    }
+    'default': dj_database_url.config(default=os.getenv("postgresql://bookbuddy_db_8id4_user:hzX7KAfw1TYwCimBsPvKySsw8Blav6V2@dpg-culg2olds78s73br59u0-a.oregon-postgres.render.com/bookbuddy_db_8id4"))
 }
 
 
